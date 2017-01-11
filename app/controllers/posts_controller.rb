0ffc272db3d0b57ce23858510aa5dd7f5d285e3d
Justine Vacher <jvacher001@etu.u-bordeaux.fr>
2017-01-11 20:07:04 +0100

    fichier edit des post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,33 +7,47 @@ class PostsController < ApplicationController
     def show
         load_post
     end
-#
-#    def edit
-#        load_user
-#        load_post
-#    end
-#
-##    def update
-##        load_user
-##        load_post
-##        if @user.update_attributes(user_params)
-##          redirect_to[':show']
-##        else
-##          redirect_to[':edit']
-##        end
-##    end
-#
-#    def new
-#        load_user
-#        @post = Post.new
-#    end
-#    
+
+    def edit
+        load_post
+    end
+
+    def update
+        load_post
+        render @post
+        if @post.update_attributes(parameters)
+            
+            
+        else
+            render 'edit'
+      
+        end
+    end
+
+    def new
+        @post = Post.new
+    end
+    def create
+        @post = Post.new(parameters) 
+
+        if @post.save
+            redirect_to[':posts']
+        else
+            redirect_to[':posts']
+        end
+    end
+    
+    def parameters
+        params.require(:post).permit(:title,:content)
+    end
+    
     protected
-#    def load_user
-#        @user = User.find params[:user_id]
-#    end
+    def load_user
+        @user = User.find params[:user_id]
+    end
     def load_post
         @post = Post.find params[:id]
     end
+    
     
 end
