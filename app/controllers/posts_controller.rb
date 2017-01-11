3dc7a00cb657ee5feec53dcab45a092569b735ac
Justine Vacher <jvacher001@etu.u-bordeaux.fr>
2017-01-11 16:19:45 +0100

    mise en place de l'architecture de posts - fichier index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,40 @@
 class PostsController < ApplicationController
+    
+    def index
+        @post = Post.all
+    end
+
+#    def show
+#        load_user
+#        load_post
+#    end
+#
+#    def edit
+#        load_user
+#        load_post
+#    end
+#
+##    def update
+##        load_user
+##        load_post
+##        if @user.update_attributes(user_params)
+##          redirect_to[':show']
+##        else
+##          redirect_to[':edit']
+##        end
+##    end
+#
+#    def new
+#        load_user
+#        @post = Post.new
+#    end
+#    
+#    protected
+#    def load_user
+#        @user = User.find params[:user_id]
+#    end
+#    def load_post
+#        @post = Post.find params[:id]
+#    end
+#    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+    
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -55,5 +57,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root to: "users#index"
+  root to: "posts#index"
 end
