a76025b2444878af28aaa008381498506b339c09
vmorreel <dawin.vmorreel@gmail.com>
2017-01-14 18:26:40 +0100

    Fixing edit user form --> linking to devise edit form

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  validates :password, presence: true
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
