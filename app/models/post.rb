859ac74249fcb81cdc2d2a2e08a0c2153f211dd4
vmorreel <dawin.vmorreel@gmail.com>
2017-01-14 18:17:04 +0100

    Fixing bugs relative to comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
   mount_uploader :thumbnail, AvatarUploader
+  validates :content, presence: true
+  validates :title, presence: true
+  validates :lead_paragraph, presence: true
   belongs_to :user
   has_many :comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   validates :password, presence: true
 
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
   
   mount_uploader :avatar, AvatarUploader
 end
